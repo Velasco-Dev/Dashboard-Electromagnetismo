1ff9619f46256a03dcbdde4c5533174ec964ddcf
Aldair <aldairtorres507@gmail.com>
2026-03-26 21:58:27 -0500

    feat: add ino

---
+++ src/ino/sketches/panel/env.ino
@@ -0,0 +1,6 @@
+#define WIFI_SSID "MOVILES"
+#define WIFI_PASSWORD "M0v1les.&2025@"
+
+#define MQTT_BROKER "broker.hivemq.com"
+#define MQTT_PORT 1883
+#define MQTT_CLIENT_ID "esp32_solar_001"
---
+++ src/ino/sketches/panel/wifi_manager.ino
@@ -0,0 +1,43 @@
+#include <WiFi.h>
+
+bool wifiConnected = false;
+int reconnectAttempts = 0;
+
+void wifiConnect() {
+  Serial.println("Conectando WiFi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  int timeout = WIFI_CONFIG_TIMEOUT;
+
+  while (WiFi.status() != WL_CONNECTED && timeout--) {
+    digitalWrite(LED_PIN, HIGH);
+    delay(100);
+    digitalWrite(LED_PIN, LOW);
+    delay(400);
+    Serial.print(".");
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    wifiConnected = true;
+    Serial.println("\nWiFi conectado");
+    Serial.println(WiFi.localIP());
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    wifiConnected = false;
+    Serial.println("\nError WiFi");
+  }
+}
+
+bool wifiCheck() {
+  return WiFi.status() == WL_CONNECTED;
+}
+
+void wifiReconnect() {
+  if (!wifiCheck()) {
+    reconnectAttempts++;
+    Serial.println("Reconectando WiFi...");
+    WiFi.disconnect();
+    delay(2000);
+    wifiConnect();
+  }
+}
